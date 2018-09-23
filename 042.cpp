741a261d17d6731f3e26a06a05873bfe2e7b8a92
chenf99 <1163874966@qq.com>
2018-09-24 00:01:54 +0800

    Update 042.cpp

--- 042.cpp
+++ 042.cpp
@@ -20,45 +20,37 @@ public:
     int trap(vector<int>& height) {
         if (height.size() <= 2) return 0;
         int result = 0;
-        stack<int> s;
         int bottom = height[0];
         int top = 0;
+		int rock = 0, bar_num = 0;
         for (int i = 1; i < height.size(); ++i) {
             if (bottom <= height[i]) {
                 //计算水量
-                int rock = 0, bar_num = 0;
-                while (!s.empty()) {
-                    int tmp = s.top();
-                    s.pop();
-                    rock += tmp;
-                    bar_num++;
-                }
                 int water = bottom * bar_num - rock;
-                //cout << i << " " << water << " " << bar_num << endl;
                 result += water;
                 bottom = height[i];
                 top = i;
+				rock = bar_num = 0;
             }
-            else s.push(height[i]);
+            else {
+				rock += height[i];
+				bar_num++;
+			}
         }
-        stack<int> s2;
         bottom = height[height.size() - 1];
+		rock = bar_num = 0;
         for (int i = height.size() - 2; i >= top; --i) {
             if (bottom <= height[i]) {
                 //计算水量
-                int rock = 0, bar_num = 0;
-                while (!s2.empty()) {
-                    int tmp = s2.top();
-                    s2.pop();
-                    rock += tmp;
-                    bar_num++;
-                }
                 int water = bottom * bar_num - rock;
-                //cout << i << " " << water << " " << bar_num << endl;
                 result += water;
                 bottom = height[i];
+				rock = bar_num = 0;
             }
-            else s2.push(height[i]);
+            else {
+				rock += height[i];
+				bar_num++;
+			}
         }
         return result;
     }
