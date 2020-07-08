e92abf8cebef8f8a09c98f6c1b38a896d7ab7a16
chenf99 <1163874966@qq.com>
2020-07-08 16:17:48 +0800

    new solution

--- 032.cpp
+++ 032.cpp
@@ -18,27 +18,75 @@ Explanation: The longest valid parentheses substring is "()()"
 #include<iostream>
 #include<string>
 #include<stack>
+#include<vector>
 using namespace std;
 
+// 解法一
+// class Solution {
+// public:
+//     int longestValidParentheses(string str) {
+//         int result = 0, start = 0;
+//         stack<int> s;
+//         for (int i = 0; i < str.size(); ++i) {
+//             if (str[i] == '(') s.push(i);
+//             else {
+//                 if (s.empty()) start = i + 1;
+//                 else {
+//                     s.pop();
+//                     if (s.empty()) result = max(result, i - start + 1);
+//                     else result = max(result, i - s.top());
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+// 解法二
 class Solution {
 public:
-    int longestValidParentheses(string str) {
-        int result = 0, start = 0;
-        stack<int> s;
-        for (int i = 0; i < str.size(); ++i) {
-            if (str[i] == '(') s.push(i);
+    int longestValidParentheses(string s) {
+        if (s.length() <= 1) return 0;
+        vector<int> dp(s.length(), 0);
+        int result = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '(') continue;
             else {
-                if (s.empty()) start = i + 1;
-                else {
-                    s.pop();
-                    if (s.empty()) result = max(result, i - start + 1);
-                    else result = max(result, i - s.top());
+                if (i >= 1 && s[i - 1] == '(') dp[i] = 2 + (i >= 2 ? dp[i - 2] : 0);
+                else if (i >= 1 && s[i - 1] == ')') {
+                    if (i - dp[i - 1] >= 1 && s[i - dp[i - 1] - 1] == '(') {
+                        dp[i] = 2 + dp[i - 1] + (i - dp[i - 1] >= 2 ? dp[i - dp[i - 1] - 2] : 0);
+                    }
                 }
+                result = max(result, dp[i]);
             }
         }
         return result;
     }
 };
+
+// 解法三
+// class Solution {
+// public:
+//     int longestValidParentheses(string s) {
+//         int left = 0, right = 0;
+//         int result = 0;
+//         for (auto i : s) {
+//             if (i == '(') left++;
+//             else right++;
+//             if (left == right) result = max(result, 2 * right);
+//             else if (right > left) left = right = 0;
+//         }
+//         left = right = 0;
+//         for (int i = s.length() - 1; i >= 0; --i) {
+//             if (s[i] == '(') left++;
+//             else right++;
+//             if (left == right) result = max(result, 2 * right);
+//             else if (left > right) left = right = 0;
+//         }
+//         return result;
+//     }
+// };
 
 int main() {
     Solution s;
