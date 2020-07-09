a7af5ea2a3f951da349bdbf5cc0a5daa9549148a
chenf99 <1163874966@qq.com>
2020-07-09 18:06:22 +0800

    new solution

--- 135.cpp
+++ 135.cpp
@@ -27,22 +27,51 @@ Explanation: You can allocate to the first, second and third child with 1, 2, 1 
 #include<numeric>
 using namespace std;
 
+// 解法一
+// class Solution {
+// public:
+//     int candy(const vector<int>& ratings) {
+//         vector<int> candy(ratings.size(), 0);
+//         //从两边开始分别遍历一次
+//         for (int i = 1; i < ratings.size(); ++i) {
+//             if (ratings[i] > ratings[i - 1]) {
+//                 candy[i] = candy[i - 1] + 1;
+//             }
+//         }
+//         for (int i = ratings.size() - 2; i >= 0; --i) {
+//             if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
+//                 candy[i] = candy[i + 1] + 1;
+//             }
+//         }
+//         return accumulate(candy.begin(), candy.end(), candy.size());
+//     }
+// };
+
+// 解法二
 class Solution {
 public:
-    int candy(const vector<int>& ratings) {
-        vector<int> candy(ratings.size(), 0);
-        //从两边开始分别遍历一次
+    int candy(vector<int>& ratings) {
+        int num = 0;
+        int old_scope = 0;
+        int up = 0, down = 0;
         for (int i = 1; i < ratings.size(); ++i) {
-            if (ratings[i] > ratings[i - 1]) {
-                candy[i] = candy[i - 1] + 1;
+            int new_scope = ratings[i] > ratings[i - 1] ? 1 : (ratings[i] < ratings[i - 1] ? -1 : 0);
+            if ((old_scope > 0 && new_scope == 0) || (old_scope < 0 && new_scope >= 0)) {
+                num += count(up) + count(down) + max(up, down);
+                up = 0;
+                down = 0;
             }
+            if (new_scope > 0) up++;
+            else if (new_scope < 0) down++;
+            else num++;
+            old_scope = new_scope;
         }
-        for (int i = ratings.size() - 2; i >= 0; --i) {
-            if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
-                candy[i] = candy[i + 1] + 1;
-            }
-        }
-        return accumulate(candy.begin(), candy.end(), candy.size());
+        num += count(up) + count(down) + max(up, down) + 1;
+        return num;
+    }
+private:
+    int count(int n) {
+        return (n * (n + 1)) / 2;
     }
 };
 
