6d6cafe9c55b836de4eb28183648451dc7a8ff41
chenf99 <1163874966@qq.com>
2020-07-11 16:54:07 +0800

    new solution

--- 128.cpp
+++ 128.cpp
@@ -16,85 +16,71 @@ Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefor
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-using std::cout;
-using std::vector;
-using std::unordered_map;
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
 
+// 解法一
+// class UnionSet {
+// private:
+//     unordered_map<int, int> parent;
+//     unordered_map<int, int> count; // count[x]:以x为root的元素个数
+// public:
+//     int max_count;
+//     UnionSet(vector<int>& nums) {
+//         max_count = 0;
+//         for (auto i : nums) {
+//             parent[i] = i;
+//             count[i] = 1;
+//             max_count = 1;
+//         }
+//     }
+//     bool exist(int x) {
+//         return parent.find(x) != parent.end();
+//     }
+//     int find(int x) {
+//         if (parent[x] == x) return x;
+//         else {
+//             parent[x] = find(parent[x]);
+//             return parent[x];
+//         }
+//     }
+//     void union_set(int x1, int x2) {
+//         int root1 = find(x1), root2 = find(x2);
+//         if (root1 != root2) {
+//             parent[root1] = root2;
+//             count[root2] += count[root1];
+//             max_count = max(max_count, count[root2]);
+//         }
+//     }
+// };
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         UnionSet set(nums);
+//         for (auto i : nums) {
+//             if (set.exist(i - 1)) set.union_set(i, i - 1);
+//             if (set.exist(i + 1)) set.union_set(i, i + 1);
+//         }
+//         return set.max_count;
+//     }
+// };
 
-class UnionFindSet {
-private:
-    int size;
-    vector<int> parent;
-    //child表示根节点的孩子数
-    vector<int> child;
-public:
-    UnionFindSet(int size) {
-        this->size = size;
-        parent = vector<int>(size);
-        child = vector<int>(size);
-    }
-
-    void makeSet() {
-        for (int i = 0; i < size; ++i) {
-            parent[i] = i;
-            child[i] = 1;
-        }
-    }
-
-    int find(int x) {
-        //查找过程中使用路径压缩
-        if (x != parent[x]) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    void Union(int x, int y) {
-        int parent_x = find(x);
-        int parent_y = find(y);
-        if (parent_x == parent_y) return;
-        if (child[parent_x] > child[parent_y]) {
-            parent[parent_y] = parent_x;
-            child[parent_x] += child[parent_y];
-        }
-        else {
-            parent[parent_x] = parent_y;
-            child[parent_y] += child[parent_x];
-        }
-    }
-
-    int max_child() {
-        int max = 1;
-        for (auto i : child) {
-            if (max < i) max = i;
-        }
-        return max;
-    }
-};
-
+// 解法二
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.size() == 0) return 0;
-        //利用hashmap来记录元素是否存在
-        //键为元素值，值为元素在并查集中的位置
-        unordered_map<int, int> map;
-        for (int i = 0; i < nums.size(); ++i) {
-            map.insert(std::make_pair(nums[i], i));
-        }
-        UnionFindSet set(nums.size());
-        set.makeSet();
-        for (auto it = map.begin(); it != map.end(); ++it) {
-            auto smaller = map.find(it->first - 1);
-            auto bigger = map.find(it->first + 1);
-            //比元素小1的元素存在
-            if (smaller != map.end()) {
-                set.Union(it->second, map[it->first - 1]);
-            }
-            //比元素小1的元素存在
-            if (bigger != map.end()) {
-                set.Union(it->second, map[it->first + 1]);
+        unordered_set<int> m;
+        int result = 0;
+        for (auto i : nums) m.insert(i);
+        for (auto i : nums) {
+            if (m.find(i - 1) == m.end()) {
+                int j = i + 1;
+                while (m.find(j) != m.end()) j++;
+                result = max(result, j - i);
             }
         }
-        return set.max_child();
+        return result;
     }
 };
 
