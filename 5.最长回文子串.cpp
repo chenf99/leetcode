d69606e5ec08b6de3d007e487446d88898985179
chenf99 <1163874966@qq.com>
2020-07-15 23:39:04 +0800

    5

---
+++ 5.最长回文子串.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=cpp
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+    }
+};
+// @lc code=end
+
