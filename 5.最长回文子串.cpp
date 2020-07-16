8ab83c6ad51cb5a5f6f62d3941f03e8a1a795ef5
chenf99 <1163874966@qq.com>
2020-07-16 15:10:26 +0800

    :tada: 5

--- 5.最长回文子串.cpp
+++ 5.最长回文子串.cpp
@@ -9,11 +9,53 @@
 #include <string>
 using namespace std;
 
+// 解法一
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
+        int n = s.length();
+        if (n == 0) return "";
+        bool isPalin[n][n];
+        string result(1, s[n - 1]);
+        for (int start = n - 1; start >= 0; --start) {
+            isPalin[start][start] = true;
+            for (int end = start + 1; end < n; ++end) {
+                isPalin[start][end] = s[start] == s[end];
+                if (end > start + 1) isPalin[start][end] = isPalin[start][end] && isPalin[start + 1][end - 1];
+                if (isPalin[start][end] && end - start + 1 > result.length()) 
+                    result = s.substr(start, end - start + 1);
+            }
+        }
+        return result;
     }
 };
-// @lc code=end
 
+// 解法二
+// class Solution {
+// public:
+//     string longestPalindrome(string s) {
+//         int start = 0, end = 0;
+//         for (int i = 0; i < s.length(); ++i) {
+//             auto [left1, right1] = expandStr(s, i, i);
+//             auto [left2, right2] = expandStr(s, i, i + 1);
+//             if (right1 - left1 > end - start) {
+//                 start = left1;
+//                 end = right1;
+//             }
+//             if (right2 - left2 > end - start) {
+//                 start = left2;
+//                 end = right2;
+//             }
+//         }
+//         return s.substr(start, end - start + 1);
+//     }
+// private:
+//     pair<int, int> expandStr(string s, int left, int right) {
+//         while (left >= 0 && right < s.length() && s[left] == s[right]) {
+//             left--;
+//             right++;
+//         }
+//         return {left + 1, right - 1};
+//     }
+// };
+// @lc code=end
