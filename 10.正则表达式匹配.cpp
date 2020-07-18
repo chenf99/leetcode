e713801b2c4627bf159dd0476f425830dc4c7fd4
chenf99 <1163874966@qq.com>
2020-07-18 22:55:43 +0800

    优化空间复杂度

--- 10.正则表达式匹配.cpp
+++ 10.正则表达式匹配.cpp
@@ -34,5 +34,36 @@ public:
         return dp[s.length()][p.length()];
     }
 };
+
+// 优化空间复杂度
+// class Solution {
+// public:
+//     bool isMatch(string s, string p) {
+//         bool dp[p.length() + 1]; // dp[j]:s前i个字符与p前j个字符是否匹配
+//         for (int j = 1; j <= p.length(); ++j) dp[j] = false;
+//         dp[0] = true;
+//         for (int i = 0; i <= s.length(); ++i) {
+//             bool leftNor = dp[0];
+//             dp[0] = i == 0 ? true : false;
+//             for (int j = 1; j <= p.length(); ++j) {
+//                 bool tmp = dp[j];
+//                 if (p[j - 1] == '*') {
+//                     if (i == 0 || s[i - 1] != p[j - 2] && p[j - 2] != '.') {
+//                         dp[j] = dp[j - 2];
+//                     }
+//                     else {
+//                         dp[j] = dp[j - 2] || dp[j];
+//                     }
+//                 }
+//                 else if (i == 0) dp[j] = false;
+//                 else {
+//                     dp[j] = leftNor && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+//                 }
+//                 leftNor = tmp;
+//             }
+//         }
+//         return dp[p.length()];
+//     }
+// };
 // @lc code=end
 
