e6b137e6bcf2ccf1eb070963851397d56901ad36
chenf99 <1163874966@qq.com>
2020-07-21 13:39:21 +0800

    new solution

--- 19.删除链表的倒数第n个节点.cpp
+++ 19.删除链表的倒数第n个节点.cpp
@@ -21,31 +21,54 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// 解法一
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         ListNode* tmp = head;
+//         int length = 0;
+//         while (tmp) {
+//             length++;
+//             tmp = tmp->next;
+//         }
+//         int index = length - n, count = 1;
+//         tmp = head;
+//         while (tmp && count < index) {
+//             tmp = tmp->next;
+//             count++;
+//         }
+//         ListNode* node;
+//         if (index > 0) {
+//             node = tmp->next;
+//             tmp->next = node->next;
+//         }
+//         else {
+//             node = head;
+//             head = head->next;
+//         }
+//         delete node;
+//         return head;
+//     }
+// };
+
+// 解法二
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* tmp = head;
-        int length = 0;
-        while (tmp) {
-            length++;
-            tmp = tmp->next;
+        if (!head) return head;
+        ListNode* dummyhead = new ListNode(-1, head);
+        ListNode *left = dummyhead, *right = dummyhead;
+        int count = 0;
+        for (int i = 0; i < n; ++i) right = right->next;
+        while (right->next) {
+            right = right->next;
+            left = left->next;
         }
-        int index = length - n, count = 1;
-        tmp = head;
-        while (tmp && count < index) {
-            tmp = tmp->next;
-            count++;
-        }
-        ListNode* node;
-        if (index > 0) {
-            node = tmp->next;
-            tmp->next = node->next;
-        }
-        else {
-            node = head;
-            head = head->next;
-        }
-        delete node;
+        ListNode* tmp = left->next;
+        left->next = tmp->next;
+        delete tmp;
+        head = dummyhead->next;
+        delete dummyhead;
         return head;
     }
 };
