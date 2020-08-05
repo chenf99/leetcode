cd9dd69c11ad191a2e16e389afe74c4b20d9c52b
chenf99 <1163874966@qq.com>
2020-08-05 12:17:46 +0800

    :tada: 106

---
+++ 106.从中序与后序遍历序列构造二叉树.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=106 lang=cpp
+ *
+ * [106] 从中序与后序遍历序列构造二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < inorder.size(); ++i) m[inorder[i]] = i;
+        return helper(m, postorder, 0, inorder.size() - 1, 0, inorder.size() - 1);
+    }
+private:
+    TreeNode* helper(unordered_map<int, int>& m, vector<int>& postorder, int l1, int r1, int l2, int r2) {
+        if (l1 > r1) return nullptr;
+        TreeNode* root = new TreeNode(postorder[r2]);
+        int index = m[postorder[r2]], left_num = index - l1;
+        root->left = helper(m, postorder, l1, l1 + left_num - 1, l2, l2 + left_num - 1);
+        root->right = helper(m, postorder, l1 + left_num + 1, r1, l2 + left_num, r2 - 1);
+        return root;
+    }
+};
+// @lc code=end
+
