ebebf04cc027d5bd112b93f6af9937dcce371201
chenf99 <1163874966@qq.com>
2020-08-14 15:55:29 +0800

    优化代码

--- 617.合并二叉树.cpp
+++ 617.合并二叉树.cpp
@@ -17,25 +17,12 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if (!t1 && !t2) return nullptr;
-        int val = 0;
-        TreeNode *left1 = nullptr, *left2 = nullptr;
-        TreeNode *right1 = nullptr, *right2 = nullptr;
-        if (t1) {
-            val += t1->val;
-            left1 = t1->left;
-            right1 = t1->right;
-        }
-        if (t2) {
-            val += t2->val;
-            left2 = t2->left;
-            right2 = t2->right;
-        }
-        TreeNode* root = new TreeNode(val);
-        root->left = mergeTrees(left1, left2);
-        root->right = mergeTrees(right1, right2);
-        return root;
+        if (!t1) return t2;
+        if (!t2) return t1;
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
     }
 };
 // @lc code=end
-
