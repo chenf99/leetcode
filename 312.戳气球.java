472469a1c0af02d3a035b78fc482c3ed8f236505
chenf99 <1163874966@qq.com>
2020-08-27 16:47:51 +0800

    :tada: new solution

--- 312.戳气球.java
+++ 312.戳气球.java
@@ -30,5 +30,30 @@ class Solution {
         return memo[left][right];
     }
 }
+
+// 解法二 dp
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] val = new int[n + 2];
+        for (int i = 1; i <= n; ++i) val[i] = nums[i - 1];
+        val[0] = val[n + 1] = 1;
+        int[][] dp = new int[n + 2][n + 2];
+        for (int i = 0; i <= n + 1; ++i) Arrays.fill(dp[i], -1);
+        for (int i = n + 1; i >= 0; --i) {
+            for (int j = i + 1; j <= n + 1; ++j) {
+                if (j == i + 1) dp[i][j] = 0;
+                else {
+                    for (int k = i + 1; k < j; ++k) {
+                        int sum = val[i] * val[k] * val[j];
+                        sum += dp[i][k] + dp[k][j];
+                        dp[i][j] = Math.max(dp[i][j], sum);
+                    }
+                }
+            }
+        }
+        return dp[0][n + 1];
+    }
+}
 // @lc code=end
 
